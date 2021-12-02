07aeb039c28130028af90dac7ffbbcc54eeeca4a
Admilson Manoel Nogueira Júnior <nogueirajr@hotmail.com>
2021-12-02 11:31:16 +0000

    New Acordiao

--- mui-acordiao/pages/index.js
+++ mui-acordiao/pages/index.js
@@ -1,7 +1,14 @@
 import Acordiao01 from './acordiao01';
+import Acordiao02 from './acordiao02';
 
 export default function SimpleAccordion() {
   return (
-    <Acordiao01/>
+    <>
+      <h3>Acordiao 01</h3>
+      <Acordiao01/>
+      <br/>
+      <h3>Acordiao 02</h3>
+      <Acordiao02/>
+    </>
   );
 }
